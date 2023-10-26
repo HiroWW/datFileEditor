8aab822caf9e3997da628059ce212322833797d9
Hiroaki <hiroaki.tokyu8500@gmail.com>
2023-10-26 11:39:50 +0900

    縦横スケールと細さ修正

--- dataWithDaen.py
+++ dataWithDaen.py
@@ -25,13 +25,14 @@ with open(dat_file, 'r') as file:
         y_values.append(y)
 
 # データを二次元座標にプロット
-plt.scatter(x_values, y_values, s=1)  # sはプロットのマーカーサイズ
+plt.scatter(x_values, y_values, s=0.5)  # sはプロットのマーカーサイズ
 
 # 楕円を描画
 a = 0.75  # 楕円の横軸の半径
 b = 0.06  # 楕円の縦軸の半径
-t = np.linspace(0, 2 * np.pi, 100)
-plt.plot(a * np.cos(t)+0.25 , b * np.sin(t), 'r-')  # 'r-'は赤色の線を意味します
+t = np.linspace(0, 2 * np.pi, 500)
+plt.axis('equal')
+plt.plot(a * np.cos(t)+0.25 , b * np.sin(t), 'r-',lw=0.5)  # 'r-'は赤色の線を意味します
 
 plt.xlabel('X')
 plt.ylabel('Y')
