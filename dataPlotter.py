e900ea960c27016d061991eabbe48ed628b8d098
Hiroaki <hiroaki.tokyu8500@gmail.com>
2023-11-09 11:34:18 +0900

    data整理

---
+++ dataPlotter.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+
+# Data from the table
+data = {
+    '1': {'x': '80%', 'y1': 3.114, 'y2': '117.86%'},
+    '2': {'x': '70%', 'y1': 3.105, 'y2': '115.90%'},
+    '3': {'x': '60%', 'y1': 3.122, 'y2': '113.63%'},
+    '4': {'x': '厳密50', 'y1': 3.070, 'y2': '112.27%'},
+    '5': {'x': '45%', 'y1': 3.214, 'y2': '109.67%'},
+    '6': {'x': '40%', 'y1': 3.265, 'y2': '108.21%'},
+    '7': {'x': '25%', 'y1': 3.487, 'y2': '103.36%'},
+    '8': {'x': '18%', 'y1': 3.62, 'y2': '100.89%'},
+    '9': {'x': '10%', 'y1': 3.813, 'y2': '97.92%'},
+}
+
+# Convert data to lists and calculate the new y values
+x_values = []
+y1_values = []
+y2_values = []
+
+for key, value in data.items():
+    # Convert the x-axis values from percentage strings to float
+    x_value = float(value['x'].replace('厳密50', '50').strip('%'))
+    x_values.append(x_value)
+    
+    # Calculate the new y1 values (3.555 divided by 100)
+    y1_values.append((value['y1'] / 3.555) * 100)
+    
+    # Convert y2 values from percentage strings to float
+    y2_value = float(value['y2'].strip('%'))
+    y2_values.append(y2_value)
+
+# Plotting the data
+plt.figure(figsize=(10, 5))
+
+# First plot
+plt.plot(x_values, y1_values, label='Y1 Values', color='blue', marker='o')
+
+# Second plot
+plt.plot(x_values, y2_values, label='Y2 Values', color='red', marker='x')
+
+# Adding titles and labels
+plt.title('Comparison of Y1 and Y2 Values')
+plt.xlabel('X Values (%)')
+plt.ylabel('Y Values (%)')
+plt.legend()
+
+# Show the plot
+plt.show()
