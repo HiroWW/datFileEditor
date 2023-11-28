fbbc248bfa34eeeb0da7a3d69b99ed1a5e1be3af
Hiroaki <hiroaki.tokyu8500@gmail.com>
2023-11-28 12:07:36 +0900

    レジュメ用の図つくった

--- illustMakerForGradHappyo.py
+++ illustMakerForGradHappyo.py
@@ -9,7 +9,7 @@ wing_shape = np.loadtxt('naca0012_sharpTE.dat')
 
 # 半径のリスト
 # radii = [1, 1.5, 2, 3, 5]  # この半径のリストを使用者が提供する
-radii = [1, 1.5, 2, 3, 5]  # この半径のリストを使用者が提供する
+radii = [1, 1.5, 2]  # この半径のリストを使用者が提供する
 
 # 色のリスト（半径ごとに異なる色を割り当てる）
 # colors = ['blue', 'red', 'green', 'magenta', 'cyan']
@@ -45,11 +45,11 @@ plot_wing_at_coordinates([(0, 0)], origin_color)
 
 # 角度5度の矢印を描画（左下から伸びる）
 arrow_angle = math.radians(5)  # 角度をラジアンに変換
-arrow_length = 3  # 矢印の長さ
-plt.arrow(-6, -1, arrow_length * math.cos(arrow_angle), arrow_length * math.sin(arrow_angle), head_width=0.2, head_length=0.3, fc='purple', ec='purple')
+arrow_length = 1  # 矢印の長さ
+plt.arrow(-2.7, -1, arrow_length * math.cos(arrow_angle), arrow_length * math.sin(arrow_angle), head_width=0.2, head_length=0.3, fc='purple', ec='purple')
 
 # 矢印に対するラベルを追加（TeX表記）
-plt.text(-6, -0.5,   r'$U_\infty$', fontsize=18, color='purple')
+plt.text(-2.7, -0.5,   r'$U_\infty$', fontsize=18, color='purple')
 
 # プロット範囲の設定
 # plt.xlim(-7, )  # x軸の範囲
@@ -62,12 +62,12 @@ def plot_angle_mark(radius, angle_deg, label):
     end_y = radius * math.sin(angle_rad)
     plt.arrow(0.5, 0, end_x, end_y, head_width=0.1, head_length=0.15, fc='black', ec='black')
     if (angle_deg == 0):
-        plt.text(end_x * 1.2, end_y * 1.1, label, fontsize=10, ha='center', va='center')
+        plt.text(end_x * 1.3, end_y * 1.1, label, fontsize=10, ha='center', va='center')
     else : 
         plt.text(end_x * 1.1, end_y * 1.1, label, fontsize=10, ha='center', va='center')
 # 角度の目盛りを45度ごとに表示
 for angle in range(0, 360, 45):
-    plot_angle_mark(6, angle, f'{angle}°')
+    plot_angle_mark(2.5, angle, f'{angle}°')
 
 # グラフの設定
 plt.axis('equal')
@@ -75,7 +75,9 @@ plt.gca().set_aspect('equal', adjustable='box')
 # plt.figure(figsize=(10, 10))  # この数字を調整してグラフのサイズを変更
 
 # グラフを保存して表示
-plt.xlim(-7.5, 7.5)
-plt.ylim(-7.5, 7.5)
+# plt.xlim(-7.5, 7.5)
+# plt.ylim(-7.5, 7.5)
+plt.ylim(-3.25, 3.25)
+plt.xlim(-3, 3.5)
 plt.savefig('naca_positions_colored.png')
 plt.show()
